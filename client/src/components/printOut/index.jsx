4b48b6f0726efea64c7d8f4fd8ede7227ba9a864
RDarrel <116413862+RDarrel@users.noreply.github.com>
2024-07-18 02:52:47 +0800

    update

--- client/src/components/printOut/index.jsx
+++ client/src/components/printOut/index.jsx
@@ -1,71 +1,113 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./print.css";
-
-const items = [
-  {
-    name: "Wood Planks Wood sdfsd Chain test",
-    price: "5,000",
-    isPerKilo: true,
-    kilo: "10 1/4",
-    hasVariant: true,
-    variant: "40/20 cm",
-    subtotal: "2000",
-  },
-  {
-    name: "Shovel",
-    isPerKilo: false,
-    price: "100",
-    quantity: "1",
-    subtotal: "1000",
-  },
-  {
-    name: "Nail (Common) ",
-    price: "3000",
-    isPerKilo: true,
-    kilo: "2 1/4",
-    subtotal: "5000",
-  },
-  {
-    name: "Paint David",
-    price: "3000",
-    isPerKilo: true,
-    kilo: "2 1/4",
-    hasVariant: true,
-    variant: "Red",
-    subtotal: "6000",
-  },
-];
+import { MDBCol, MDBRow } from "mdbreact";
 
 const PrintOut = () => {
-  const printRef = useRef();
+  const [products, setProducts] = useState([]);
+  const [obj, setObj] = useState({});
+
+  const formatterSold = (data, isPerKilo) => {
+    const { quantity = 0, kilo = 0, kiloGrams = 0 } = data;
+
+    const getGrams = (grams) => {
+      switch (grams) {
+        case 0.25:
+          return "1/4";
+        case 0.5:
+          return "1/2";
+
+        default:
+          return "3/4";
+      }
+    };
+    if (isPerKilo) {
+      if (kilo > 0 && kiloGrams > 0) {
+        return { sold: `${kilo} ${getGrams(kiloGrams)}` };
+      }
+
+      if (kilo > 0 && kiloGrams === 0) {
+        return { sold: kilo };
+      }
+
+      if (kilo === 0 && kiloGrams > 0) {
+        return { sold: getGrams(kiloGrams) };
+      }
+    } else {
+      return { sold: quantity };
+    }
+  };
+
+  const formatedData = useCallback((datas) => {
+    console.log("running");
+    return datas.map((data) => {
+      const { product } = data;
+      const { isPerKilo } = product;
+
+      return { ...data, ...formatterSold(data, isPerKilo) };
+    });
+  }, []);
 
   useEffect(() => {
-    // Trigger the print dialog after a short delay to ensure content is rendered
+    const fakeDb = localStorage.getItem("collection");
+    if (!fakeDb) return false;
+    const data = JSON.parse(fakeDb);
+    setProducts(formatedData(data.purchases));
+    setObj(data);
+  }, [formatedData]);
+
+  useEffect(() => {
     const timer = setTimeout(() => {
       window.print();
-    }, 1000);
+      window.close();
+    }, 90);
 
     return () => clearTimeout(timer);
   }, []);
 
+  const getCurrentFormattedDate = () => {
+    const today = new Date();
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // getMonth() returns month from 0-11
+    const day = String(today.getDate()).padStart(2, "0");
+    const year = today.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  };
+
+  const getCurrentFormattedTimeAMPM = () => {
+    const today = new Date();
+
+    let hours = today.getHours();
+    const minutes = String(today.getMinutes()).padStart(2, "0");
+
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours %= 12;
+    hours = hours || 12;
+
+    return `${hours}:${minutes} ${ampm}`;
+  };
+
   return (
-    <div ref={printRef} className="container-receipt mt-1">
+    <div className="container-receipt mt-1">
       <div className="header">
-        <h6 className="store">Liberty Hardware</h6>
+        <h6 className="store">Liberty Hardwares</h6>
         <h6 className="address">Conception Gen. Tinio Nueva Ecija</h6>
       </div>
       <div className="sub-header">
         <h6 className="m-0 p-0">
-          Invoice No: <span className="invoice">3835067</span>
+          Invoice No: <span className="invoice">{obj?.invoice_no}</span>
         </h6>
         <div className="d-flex justify-content-between align-items-center">
           <div>
             <span className="mr-1">Date:</span>
-            <span className="font-weight-bold">10/23/2022</span>
+            <span className="font-weight-bold">
+              {getCurrentFormattedDate()}
+            </span>
           </div>
           <div>
             <span>Time:</span>
-            <span className="font-weight-bold time">10:23 Am</span>
+            <span className="font-weight-bold time">
+              {getCurrentFormattedTimeAMPM()}
+            </span>
           </div>
         </div>
       </div>
@@ -79,30 +121,28 @@ const PrintOut = () => {
             <span>Sub-total</span>
           </div>
         </div>
-        {items.map((item, index) => {
-          const { isPerKilo, quantity, kilo, price } = item;
+        {products.map((item, index) => {
+          const { srp = 0, product, variant = "", sold = 1, subtotal } = item;
           return (
             <div key={index}>
               <div className="d-flex justify-content-between w-100">
                 <div className="receipt-product-container">
                   <span className="text-start item-name">
-                    {item.name.toUpperCase()}
+                    {product.name.toUpperCase()}
                   </span>
                 </div>
               </div>
               <div className="d-flex justify-content-between align-items-center product-details-receipt">
-                <span className={`text-nowrap qty-kilo`}>
-                  {isPerKilo ? kilo : quantity}
-                </span>
-                {item.hasVariant && (
+                <span className={`text-nowrap qty-kilo ml-1`}>{sold}</span>
+                {product.hasVariant && (
                   <div className="receipt-variant">
-                    <span>{item.variant}</span>
+                    <span>{variant}</span>
                   </div>
                 )}
-                <span>{price}.00</span>
+                <span>{srp.toLocaleString()}.00</span>
 
                 <div className="subtotal-receipt d-flex justify-content-end">
-                  <span>{item.subtotal}.00</span>
+                  <span>{subtotal.toLocaleString()}.00</span>
                 </div>
               </div>
             </div>
@@ -111,24 +151,40 @@ const PrintOut = () => {
         <div className="d-flex justify-content-end mt-2 mb-2 ">
           <div>
             <span>Total:</span>
-            <span className="font-weight-bold ml-4">220.00</span>
+            <span className="font-weight-bold ml-4">
+              {obj.total?.toLocaleString()}.00
+            </span>
           </div>
         </div>
         <div className="footer-receipt">
-          <div className="d-flex justify-content-end cash">
-            <div>
-              <span className="mr-5 font-weight-bold">Cash</span>
-              <span className="font-weight-bold ml-4 cash">22000.00</span>
-            </div>
-          </div>
-          <div className="d-flex justify-content-end">
-            <div>
-              <span className="mr-5 font-weight-bold mr-5 change-label ">
-                Change
+          <MDBRow>
+            <MDBCol
+              md="5"
+              sm="3"
+              className="d-flex justify-content-end  cash-label"
+            >
+              <span className="mr-2 font-weight-bold ">Cash</span>
+            </MDBCol>
+            <MDBCol className="mr-4  d-flex justify-content-end">
+              <span className="font-weight-bold ml-4  mr-3 cash-value">
+                {obj.cash?.toLocaleString()}.00
               </span>
-              <span className="font-weight-bold  change">220.00</span>
-            </div>
-          </div>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol
+              md="5"
+              sm="3"
+              className="d-flex justify-content-end  cash-label"
+            >
+              <span className="mr-2 font-weight-bold ">Change</span>
+            </MDBCol>
+            <MDBCol className="mr-4 d-flex justify-content-end">
+              <span className="font-weight-bold ml-4  mr-3 cash-value">
+                {Number(obj.cash - obj.total || 0)?.toLocaleString()}.00
+              </span>
+            </MDBCol>
+          </MDBRow>
         </div>
         <div className="text-center">
           <span>THANK YOU AND COME AGAIN.</span>
